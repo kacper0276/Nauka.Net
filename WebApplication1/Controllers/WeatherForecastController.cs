1b7de913dc09ba2192e5f953c7d7f3e7423b7369
kacper0276 <kacper0276@op.pl>
2023-11-02 15:26:44 +0100

    Dodaj pliki projektów.

---
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private readonly IWeatherForecastService _service;
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IWeatherForecastService service)
+        {
+            _logger = logger;
+            _service = service;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var result = _service.Get();
+            return result;
+        }
+    }
+}
