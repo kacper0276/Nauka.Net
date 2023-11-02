bd74093835eb5f6519bd884f64d75f03bbc86a92
kacper0276 <kacper0276@op.pl>
2023-11-02 17:15:02 +0100

    Tworzenie Bazy Danych

---
+++ WebApplication1/Entities/RestaurantDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Entities
+{
+    public class RestaurantDbContext : DbContext
+    {
+        private string _connectionString = "Server=(localdb)\\mssqllocaldb;Database=RestaurantDb;Trusted_Connection=True;";
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Dish> Dishes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Restaurant>()
+                .Property(r => r.Name)
+                .IsRequired()
+                .HasMaxLength(25); // Wymagana i 25 znaków
+
+            modelBuilder.Entity<Dish>()
+                .Property(d => d.Name)
+                .IsRequired(); // Pole name w Dish wymagane
+        }
+
+        // Konfiguracja połączenia do bazy danych
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
